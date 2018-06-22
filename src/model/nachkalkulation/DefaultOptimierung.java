67fae067e349e26ee87a3a9d4318d52cc47a2d5b
simon-frank <38725429+simon-frank@users.noreply.github.com>
2018-06-22 14:26:34 +0100

    Add files via upload

---
+++ src/model/nachkalkulation/DefaultOptimierung.java
@@ -0,0 +1,16 @@
+package nachkalkulation;
+/**
+ * Diese Klasse wird verwendet, wenn der User keine eigene Klasse für die Nachkalkulation verwendet
+ *
+ *
+ */
+public class DefaultOptimierung implements Optimieren_I {
+
+	@Override
+	public int[] optimieren(int realTime, int calcTime, int functionPoints, int[] factors) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
---
+++ src/model/nachkalkulation/Optimieren_I.java
@@ -0,0 +1,5 @@
+package nachkalkulation;
+
+public interface Optimieren_I {
+	int[] optimieren(int realTime, int calcTime, int functionPoints, int[] factors);
+}
